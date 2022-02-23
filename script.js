d1a24818ad5c5b26870bf91af078e0245b6b01a9
SalamaAlmheiri <84767210+SalamaAlmheiri@users.noreply.github.com>
2022-02-23 22:15:18 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+let insideOfBridge= document.getElementById("Inside")
+
+insideOfBridge.addEventListener("click", () => {
+  document.getElementById("textbox").innerText = "You've clicked on the inside of the bridge!"
+})
